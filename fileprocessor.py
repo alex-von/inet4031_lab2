b4509920fc50b1fdf998c4535bf1836644c1032f
alex-von <alex.vongseng@gmail.com>
2023-09-15 15:09:13 +0000

    Process File Data with Python

---
+++ fileprocessor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+f = open("fileprocessor.input", "r")
+
+print("Printing out User Data:\n")
+
+for x in f:
+    words = x.replace('\n','').split(':')
+    if words[0][0] == '#':
+        print(words[0][1:] + " is skipped because it starts with a hashtag (is commented out")
+    else:
+        print("The user " + words[0] + " has a password of " + words[1] + " and has userid " + words[2] + " and groupid " + words[3])
+
+print("\nEnd of User Data")
+
+f.close()
